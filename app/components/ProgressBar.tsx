fe7784cdf6f94b1cdaeaa38d4849aba5e02cd8a0
Ajay <ajaykaloniya@gmail.com>
2026-04-05 21:45:23 +0530

    fix: progress bar

--- app/components/ProgressBar.tsx
+++ app/components/ProgressBar.tsx
@@ -1,10 +1,12 @@
 export default function ProgressBar ({current, total}:any){
     const percentage = ((current+1)/total)*100;
+    console.log(percentage)
     return (
         <div className="progress-bar">
             <div
             className="progress-percentage"
-            style={{width: `${percentage}`}}/>
+            style={{width: `${percentage}%`}}/>
+            <span className="progress-text">{percentage}%</span>
         </div>
     )
 }
